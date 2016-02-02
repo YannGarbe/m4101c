ca6153fee1690842de6de85a648abfd86ed846a7
Yann Garbé <yanngarbe@gmail.com>
2016-02-02 09:26:27 +0100

    Ajout de la fonction creerSocket : Normalement, elle crée la socket selon le domaine, le type et le protocole. Je suis pas sur de cette fonction, cependant je n'ai pas d'autres réponses pour le moment.

--- webserver/main.c
+++ webserver/main.c
@@ -5,7 +5,7 @@
 #include <netinet/tcp.h> 
 #include <unistd.h>
 
-int socket(int domain, int type, int protocol);
+int creerSocket(int domain, int type, int protocol);
 
 int main ( int argc , char ** argv )
 {
@@ -14,16 +14,9 @@ int main ( int argc , char ** argv )
 	}
 	printf ("OK\n");
 
-	int socket_serveur;
 	/*Création de la socket*/
-	socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
-	if (socket_serveur != -1)
-	{
-		printf("%d\n", socket_serveur);
-		perror("socket_serveur");
-		/* traitement de l'erreur*/
-	}
-	close(socket_serveur);
+	int testSocket = creerSocket(AF_INET, SOCK_STREAM, 0);
+	close(testSocket);
 	/*Fermeture de la socket serveur*/
 
 		return 0;
--- webserver/socket.c
+++ webserver/socket.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+
+int socket(int domain, int type, int protocol);
 
 void detailleSocket(int domain, int type, int protocol)
 {
@@ -15,4 +20,16 @@ int socket(int domain, int type, int protocol)
 	return domain+type+protocol;
 }
 
+int creerSocket(int domain, int type, int protocol)
+{
+	int socket_serveur = socket(domain, type, protocol);
+	if (socket_serveur != -1)
+	{
+		printf("%d\n", socket_serveur);
+		perror("socket_serveur");
+		/* traitement de l'erreur*/
+	}
+	/*Utilisation de la socket*/
+	return socket_serveur;
+}
 

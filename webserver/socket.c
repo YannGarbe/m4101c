44c1e35278ca3f3efbfbd3379bb37da926663271
Yann Garbé <yanngarbe@gmail.com>
2016-03-15 11:14:43 +0100

    Get est réussie! Pour réussir la connection, il faut ajouter EN PREMIERE LIGNE 'GET / HTTP/1.1' ou 1.0, puis appuyer 2 fois sur entrée. Dans le cas contraire, une erreur 'Bad request' est affichée.

--- webserver/socket.c
+++ webserver/socket.c
@@ -11,6 +11,14 @@
 #include <string.h> 
 #include <signal.h>
 
+char* concatenation(char *s1, char *s2)
+{
+    char *result = malloc(strlen(s1)+strlen(s2)+1);
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
 
 void traitement_signal ( int sig )
 {
@@ -37,6 +45,58 @@ void initialiser_signaux(void)
 	}
 
 }
+
+char * req(char * buf, FILE * file, int taille) {
+
+	int i;
+	for(i = 0; i<taille; i++) {
+		buf[i] = '\0';
+	}
+
+	fgets(buf, taille, file); 
+	fflush(stdout);
+
+	if(buf[0] == '\0')
+	{
+		perror("fgets");
+		exit(1);
+	}
+	return buf;
+}
+
+int verif(const char * message){
+	
+	int nb_mots=0;
+	unsigned int i = 0;
+	char * delim;
+	char * buf;
+
+	buf= strdup(message);
+	delim = strtok(buf," ");
+
+	if(strncmp(delim, "GET", 4) != 0)
+		return -1;
+
+	while(i<strlen(message)){
+		if(message[i]== ' ')
+			nb_mots++;
+		i++;
+    }
+		if(nb_mots!=2)
+			return -1;
+
+		delim = strtok(NULL," ");
+		if(strcmp("/",delim)!=0)
+			return 404;
+
+		delim = strtok(NULL," ");
+	if((strcmp(delim, "HTTP/1.0\n") != 0) && (strcmp(delim, "HTTP/1.1\n") != 0))	
+		return -1;
+
+	return 0;
+
+}
+
 
 
 int creer_serveur(int port) {
@@ -80,13 +140,17 @@ int creer_serveur(int port) {
 	
 
 	/* On peut maintenant dialoguer avec le client */
-	//const char *message_bienvenue = " Bonjour , bienvenue sur mon serveur. Ce serveur est un projet d'un étudiant de Lille 1 dans le module 'Prog Sys'. \n Mon nom est Yann Garbé et je suis le créateur de ce serveur. Bon pour l'instant, il ne sert pas à grand chose, je vous l'accorde mais j'espère qu'il grandira.\n Avant tout, merci d'utiliser ce serveur ! \n Aussi, ce projet est disponible sur GitHub ici : 'https://github.com/YannGarbe'. Vous pouvez consulter  la page si vous la voulez mais il n'y a pas grand chose.\n Petit poème : \nA travers les soupirs, les plaintes et le râle\nPoursuivons jusqu’au bout la funèbre spirale\nDe ses détours maudits.\nNotre guide n’est pas Virgile le poëte,\nLa Béatrix vers nous ne penche pas la tête\nDu fond du paradis.\n\nPour guide nous avons une vierge au teint pâle\nQui jamais ne reçut le baiser d’or du hâle\nDes lèvres du soleil.\nSa joue est sans couleur et sa bouche bleuâtre,\nLe bouton de sa gorge est blanc comme l’albâtre,\nAu lieu d’être vermeil.\n\nUn souffle fait plier sa taille délicate ;\nSes bras, plus transparents que le jaspe ou l’agate,\nPendent languissamment ;\nSa main laisse échapper une fleur qui se fane,\nEt, ployée à son dos, son aile diaphane\nReste sans mouvement.\n\nPlus sombres que la nuit, plus fixes que la pierre,\nSous leur sourcil d’ébène et leur longue paupière\nLuisent ses deux grands yeux,\nComme l’eau du Léthé qui va muette et noire,\nSes cheveux débordés baignent sa chair d’ivoire\nA flots silencieux.\n\nDes feuilles de ciguë avec des violettes\nSe mêlent sur son front aux blanches bandelettes,\nChaste et simple ornement ;\nQuant au reste, elle est nue, et l’on rit et l’on tremble\nEn la voyant venir ; car elle a tout ensemble\nL’air sinistre et charmant.\n\nQuoiqu’elle ait mis le pied dans tous les lits du monde,\nSous sa blanche couronne elle reste inféconde\nDepuis l’éternité.\nL’ardent baiser s’éteint sur sa lèvre fatale,\nEt personne n’a pu cueillir la rose pâle\nDe sa virginité.\n\nThéophile Gautier, La comédie de la mort\n\n" ;
+	const char *message_bienvenue = " Bonjour , bienvenue sur mon serveur. Ce serveur est un projet d'un étudiant de Lille 1 dans le module 'Prog Sys'. \n Mon nom est Yann Garbé et je suis le créateur de ce serveur. Bon pour l'instant, il ne sert pas à grand chose, je vous l'accorde mais j'espère qu'il grandira.\n Avant tout, merci d'utiliser ce serveur ! \n Aussi, ce projet est disponible sur GitHub ici : 'https://github.com/YannGarbe'. Vous pouvez consulter  la page si vous la voulez mais il n'y a pas grand chose.\n Petit poème : \nA travers les soupirs, les plaintes et le râle\nPoursuivons jusqu’au bout la funèbre spirale\nDe ses détours maudits.\nNotre guide n’est pas Virgile le poëte,\nLa Béatrix vers nous ne penche pas la tête\nDu fond du paradis.\n\nPour guide nous avons une vierge au teint pâle\nQui jamais ne reçut le baiser d’or du hâle\nDes lèvres du soleil.\nSa joue est sans couleur et sa bouche bleuâtre,\nLe bouton de sa gorge est blanc comme l’albâtre,\nAu lieu d’être vermeil.\n\nUn souffle fait plier sa taille délicate ;\nSes bras, plus transparents que le jaspe ou l’agate,\nPendent languissamment ;\nSa main laisse échapper une fleur qui se fane,\nEt, ployée à son dos, son aile diaphane\nReste sans mouvement.\n\nPlus sombres que la nuit, plus fixes que la pierre,\nSous leur sourcil d’ébène et leur longue paupière\nLuisent ses deux grands yeux,\nComme l’eau du Léthé qui va muette et noire,\nSes cheveux débordés baignent sa chair d’ivoire\nA flots silencieux.\n\nDes feuilles de ciguë avec des violettes\nSe mêlent sur son front aux blanches bandelettes,\nChaste et simple ornement ;\nQuant au reste, elle est nue, et l’on rit et l’on tremble\nEn la voyant venir ; car elle a tout ensemble\nL’air sinistre et charmant.\n\nQuoiqu’elle ait mis le pied dans tous les lits du monde,\nSous sa blanche couronne elle reste inféconde\nDepuis l’éternité.\nL’ardent baiser s’éteint sur sa lèvre fatale,\nEt personne n’a pu cueillir la rose pâle\nDe sa virginité.\n\nThéophile Gautier, La comédie de la mort\n\n" ;
 
-	char buffer [80];
+	char buffer [512];
 	sleep(1);
 	int pid;
 	FILE * file;
+	int erreur = 1;
 
+	char * msgError="\nHTTP/1.1 400 Bad Request\n\rConnection: close\n\rContent-Length: 17 \n\n\r400 Bad request\n\r\n";
+	
+	char * msgSuccess="\nHTTP/1.1 200 OK\n\rContent-Length: \n\n";
 	initialiser_signaux();
 	while ((socket_client = accept(socket_serveur, NULL, NULL)) != -1) 
 	{
@@ -101,12 +165,34 @@ int creer_serveur(int port) {
 		if (pid == 0)
 		{
 			
-			close(socket_serveur);
 			//write ( socket_client, message_bienvenue , strlen(message_bienvenue));
-			while(fgets(buffer, 80, file) != NULL) 
+			/*while(fgets(buffer, 512, file) != NULL) 
+			{*/
+			char * msg;
+			while((msg=req(buffer, file, sizeof(buffer)/sizeof(buffer[0]))))
 			{	
 				//fprintf(file, "<mygaServer> %s", buffer);
-				printf("%s\n", buffer);	
+				//printf("%s\n", buffer);	
+				if(erreur==1 && ((strcmp(msg,"\r\n")==0)||(strcmp(msg,"\n")==0)))
+				{
+					fprintf(file, msgError);
+				}
+				else if(erreur==0 && ((strcmp(msg,"\r\n")==0)||(strcmp(msg,"\n")==0)))
+				{
+							char str[15];
+							sprintf(str, "%d", (int)strlen(message_bienvenue));
+							char * message_end = concatenation(str,"\n\n\r200 OK\n\r");
+							msgSuccess = concatenation(msgSuccess,message_end);
+							fprintf(file, msgSuccess);
+							msgSuccess="\nHTTP/1.1 200 OK\n\rContent-Length: ";
+							printf("Connection Accomplie!\n");
+							erreur=1;
+							fprintf(file, message_bienvenue);
+				}
+				if(verif(msg)==0)
+				{
+					erreur=0;
+				}
 			}	
 			fclose(file);
 			exit (0);
